d056eee561a60f9c75ef10b0b211ccb7b2f2cad3
unknown <esc.anonimo.1@gmail.com>
2024-01-03 05:12:29 -0600

    como cuando te da

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 // Hacer un árbol binario de búsqueda
 // Usar strcmp para ubicar palabras mayores y menores
@@ -13,6 +14,52 @@
 
 
 int main(int argn, char args[]){
+    setlocale(LC_ALL, "es_MX.utf8");
+
+    Arbol diccionario;
+    Initialize(&diccionario);
+
+    char command[10];
+
+    while (strcmp(command, "salir")){
+        fgets(command, 100, stdin);
+        command[strcspn(command, "\n")] = 0;
+
+        if (!(strcmp(command, "cargar"))){
+            char dir[100];
+            printf("Nombre del archivo a leer: ");
+            fgets(dir, 100, stdin);
+            dir[strcspn(dir, "\n")] = 0;
+            LeerArchivo(&diccionario, dir);
+        }
+        else if (!(strcmp(command, "agregar"))){
+            //
+        }
+        else if (!(strcmp(command, "buscar"))){
+            char busqueda[101];
+            fgets(busqueda, 100, stdin);
+            busqueda[strcspn(busqueda, "\n")] = 0;
+            element aux;
+            strcpy(aux.palabra, busqueda);
+            element def = BuscarPalabra(&diccionario, aux);
+            printf("%s\n", def.definicion);
+        }
+        else if (!(strcmp(command, "modificar"))){
+            //
+        }
+        else if (!(strcmp(command, "eliminar"))){
+            //
+        }
+        else if (!(strcmp(command, "no se"))){
+            //
+        }
+        else if (!(strcmp(command, "help"))){
+            //
+        }
+    }
+
+
+    /*
     Arbol nose;
     Initialize(&nose);
 
@@ -22,6 +69,7 @@ int main(int argn, char args[]){
     strcpy(e.palabra, "Sucesión convergente");
     element def = BuscarPalabra(&nose, e);
     printf("Definicion: %s\n", def.definicion);
+    */
 
     return 0;
 }
